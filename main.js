1c71a0c5e5448e535a61d4a81d6508933293a93d
ATBarria <146737610+ATBarria@users.noreply.github.com>
2024-09-29 17:07:13 +0200

    third task done

--- main.js
+++ main.js
@@ -11,4 +11,32 @@ function frontDoorPassword(word = "") {
 
 frontDoorPassword("DOG");
 
-// Task 3
+// Task 3 done. It was much easier. I did the hard version using arrays
+
+const CODE_WORK = [
+  "Compilers intensily bestow",
+  "On commencing without ego",
+  "Different processes ajar",
+  "Exit with zero quick",
+];
+
+const expectedLetters = ["w", "o", "r", "k"];
+const items = [];
+CODE_WORK.forEach((item) => {
+  const index = item.length;
+  console.log(items.push(item[index - 1]));
+});
+
+console.log(items);
+
+export function backDoorResponse(line = []) {
+  const items = [];
+  line.forEach((item) => {
+    const index = item.length;
+    items.push(item[index - 1]);
+  });
+  console.log(items);
+  return items;
+}
+
+backDoorResponse("hallo");
